81d26b8373342992d647ebfc9022f8e5a18a103f
hhaemin <ahrgoals@naver.com>
2024-07-12 17:23:59 +0900

    ✨여러 개의 state 속성 다루기
    Action에 payload 전달하기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,24 +2,33 @@ import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
-  const derementHandler = () => {
+
+  const increase10Handler = () => {
+    dispatch({ type: "increase", amount: 10 });
+  };
+
+  const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>토글 카운터</button>
       <button onClick={incrementHandler}>숫자 증가</button>
-      <button onClick={derementHandler}>숫자 감소</button>
+      <button onClick={increase10Handler}>숫자 10 증가</button>
+      <button onClick={decrementHandler}>숫자 감소</button>
     </main>
   );
 };
